3ababa4dc0686f980ae7f9cd7ec0b92d83e7f03e
NegarMadadi <madadi.negar@gmail.com>
2022-03-30 10:25:07 +0200

    created variables.

---
+++ src/main/java/org/example/Variables.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myFirstNumber = (10 + 3) + (2 * 3);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal -1000 ;
+
+        System.out.println(myLastOne);
+    }
+}
