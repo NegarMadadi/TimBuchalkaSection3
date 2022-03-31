8ad9ceabe7c924ea669ceeb90bd388e795a6a0c1
NegarMadadi <madadi.negar@gmail.com>
2022-03-31 16:27:34 +0200

    created charAndBoolean.

---
+++ src/main/java/org/example/charAndBoolean.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class charAndBoolean {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char uniCode = '\u0044';
+        char anotherUniCode = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(uniCode);
+        System.out.println(anotherUniCode);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOver21 = true;
+
+
+
+
+    }
+}
