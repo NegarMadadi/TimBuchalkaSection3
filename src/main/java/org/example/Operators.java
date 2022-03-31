4d9c704612a66f405d142141b00dfb558179434a
NegarMadadi <madadi.negar@gmail.com>
2022-03-31 20:42:16 +0200

    Added some features to Operators.java.

--- src/main/java/org/example/Operators.java
+++ src/main/java/org/example/Operators.java
@@ -18,15 +18,15 @@ public class Operators {
         System.out.println("20/5 = " + result);
 
         result = result % 3;
-        System.out.println("4%3 = "+ result);
+        System.out.println("4%3 = " + result);
 
         //result + 1;
         result++; // 1+1 = 2
-        System.out.println("1+1 = "+ result);
+        System.out.println("1+1 = " + result);
 
         //resul -1
         result--; // 2-1 = 1
-        System.out.println("2-1 = "+ result) ;
+        System.out.println("2-1 = " + result);
 
         // result +2
         result += 2; // 1+2 =3
@@ -34,14 +34,56 @@ public class Operators {
 
         //result = result * 10
         result *= 10; // 3*10 = 30
-        System.out.println("3*10 = "+ result);
+        System.out.println("3*10 = " + result);
 
         //result = result * 10
         result /= 3; // 30/3 = 10
-        System.out.println("30/3 = "+ result);
+        System.out.println("30/3 = " + result);
 
         //result = result - 2
         result -= 2; // 10-2 = 8
-        System.out.println("10-2 = "+ result);
+        System.out.println("10-2 = " + result);
+
+        boolean isALien = false;
+        if (isALien == true) {
+            System.out.println("It is not an Alien!");
+            System.out.println("And I am scared of aliens.");
+        }
+
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high Score!");
+        }
+
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than secondTopScore and less than topScore");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true.");
+        }
+
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is is true.");
+
+
+            boolean isCAr = false;
+            if (isCAr){
+                System.out.println("This is not supposed to happen.");
+            }
+
+            isCAr = true;
+            boolean wasCar = isCAr ? true : false;
+            if (wasCar){
+                System.out.println("wasCar is true.");
+            }
+
+
+
+        }
     }
 }
+
